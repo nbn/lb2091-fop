b7e5bc5de969ff42a3332bf3b6686ca3c5a8d3f3
Niels Bech Nielsen <niels.bech.nielsen@maersk.com>
2019-01-11 08:06:42 +0100

    Initial commit

---
+++ src/test/java/dk/lundogbendsen/foundation/FoundationApplicationTests.java
@@ -0,0 +1,17 @@
+package dk.lundogbendsen.foundation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FoundationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
